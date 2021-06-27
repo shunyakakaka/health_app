1714c538de0fa6b8b5cb1785612b5e70b5c21e6c
shunyasaitama <85655151+shunyasaitama@users.noreply.github.com>
2021-06-27 11:30:17 +0900

    ユーザー機能追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all 
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "ユーザー「#{@user.name}」を登録しました。"
+      redirect_to users_path
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  def destroy
+    User.delete(params[:id])
+    binding.pry
+    redirect_to users_path
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.new(user_params)
+    @user.update
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show, :new, :edit, :create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :boards
+  resources :boards, only: [:index, :show, :new, :update, :create, :edit]
 end
